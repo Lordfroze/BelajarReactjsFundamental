8b5662769cef5774c28db6317505738911bf4208
yoga <yoga@yogas-MacBook-Pro.local>
2026-02-24 18:04:05 +0700

    Fitur Filter Data dan Pengenalan useState

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,12 +1,31 @@
+import React, { useState } from "react"; // Import useState hook from React
 import posts from "../posts.json";
 import Article from "../components/Arcticle";
 
 function Homepage() {
+  // membuat state search
+  const [search, setSearch] = useState("");
+
+  // membuat function changeSearch
+  const changeSearch = (event) => {
+    setSearch(event.target.value);
+  };
+
   return (
     <>
       <h1>Simple blog</h1>
-      {posts.map((blog) => (
-        <Article title={blog.title} tags={blog.tags} date={blog.date} />
+      {/* Membuat input search */}
+      <div>
+        Cari Artikel : <input onChange={changeSearch}></input>
+      </div>
+      <small>Ditemukan 0 data dengan pencarian kata {search}</small>
+      {posts.map((blog, index) => (
+        <Article
+          title={blog.title}
+          tags={blog.tags}
+          date={blog.date}
+          key={index}
+        />
       ))}
     </>
   );
