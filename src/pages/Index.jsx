3e435796253f4e07131e1b0527f59f39199050a5
yoga <yoga@yogas-MacBook-Pro.local>
2026-02-25 17:57:52 +0700

    Mengembalikan Value ke Parent melalui Props

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,16 +1,20 @@
 import { useState } from "react"; // Import useState hook from React
-import postsData from "../posts.json";
+import postsData from "../posts.json"; // Import posts data from JSON file
 import Article from "../components/Arcticle";
-import Search from "../components/Search";
+import Search from "../components/Search"; // Import Search component from Search.jsx file
 
 function Homepage() {
+  // membuat state posts dengan nilai awal postsData
   const [posts, setPosts] = useState(postsData);
 
+  // membuat function onSearchChange yang menerima value
   const onSearchChange = (value) => {
     console.log(value);
+    // membuat filteredPosts yang berisi postsData yang title-nya mengandung value
     const filteredPosts = postsData.filter((item) =>
       item.title.includes(value),
     );
+    // mengupdate state posts dengan nilai filteredPosts
     setPosts(filteredPosts);
   };
   return (
