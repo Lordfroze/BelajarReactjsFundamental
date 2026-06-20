5b20f56270163bf567df8ab4d85dce6eebb77458
lordfroze <yogaadipratama22@gmail.com>
2026-06-21 04:31:33 +0700

    layouting

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -1,14 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
+import RootLayout from "../layouts/RootLayout";
+
 import Home from "../pages/index";
 import About from "../pages/About";
 
 export const router = createBrowserRouter([
   {
-    path: "/home",
-    element: <Home />,
-  },
-  {
-    path: "/about",
-    element: <About />,
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
   },
 ]);
