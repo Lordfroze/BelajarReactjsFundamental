08a14dd749ff67bc423d1aa9b1205fe0ff57a5f9
yoga <yoga@yogas-MacBook-Pro.local>
2026-06-21 13:13:41 +0700

    done react fundamental

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,10 +1,39 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
+import "../styles/index.css";
 
 function RootLayout() {
   return (
     <>
-      <Link to="/">Home</Link> | <Link to="/blog">Blogs</Link> |
-      <Link to="/about">About</Link>
+      <NavLink
+        className={({ isActive }) => (isActive ? "active" : "")}
+        style={{
+          marginRight: "10px",
+        }}
+        to="/"
+      >
+        Home
+      </NavLink>{" "}
+      |{" "}
+      <NavLink
+        className={({ isActive }) => (isActive ? "active" : "")}
+        style={{
+          marginRight: "10px",
+          marginLeft: "10px",
+        }}
+        to="/blog"
+      >
+        Blogs
+      </NavLink>{" "}
+      |
+      <NavLink
+        className={({ isActive }) => (isActive ? "active" : "")}
+        style={{
+          marginLeft: "10px",
+        }}
+        to="/about"
+      >
+        About
+      </NavLink>
       <p />
       <Outlet />
     </>
